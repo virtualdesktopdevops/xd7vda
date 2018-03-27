0b6a7ae49699f61d0b10110dd6c51b3881ff2126
matt6697 <32440697+matt6697@users.noreply.github.com>
2018-03-27 22:12:03 +0200

    Updated network interface config and puppet dependancies

--- manifests/w10networkconfig.pp
+++ manifests/w10networkconfig.pp
@@ -1,17 +1,19 @@
 #Class configuring network for a Windows 10 Desktop VDA
 class xd7vda::w10networkconfig inherits xd7vda {
   if ($facts['osfamily'] == 'windows') and ($facts['os']['release']['major']==10) {
-    #Disable NETBIOS
-    dsc_xnetbios{'DisableNetBIOS':
-      dsc_interfacealias => 'Ethernet',
-      dsc_setting        => 'Disable'
-    }
+    $facts['networking']['interfaces'].each |$netinterface| {
+      #Disable NETBIOS
+      dsc_xnetbios{"DisableNetBIOS_${netinterface}":
+        dsc_interfacealias => $netinterface,
+        dsc_setting        => 'Disable'
+      }
 
-    #Disable IPV6
-    dsc_xnetadapterbinding{'DisableIPv6':
-      dsc_interfacealias => 'Ethernet',
-      dsc_componentid    => 'ms_tcpip6',
-      dsc_state          => 'Disabled'
+      #Disable IPV6
+      dsc_xnetadapterbinding{"DisableIPv6_${netinterface}":
+        dsc_interfacealias => $netinterface,
+        dsc_componentid    => 'ms_tcpip6',
+        dsc_state          => 'Disabled'
+      }
     }
   }
 }
