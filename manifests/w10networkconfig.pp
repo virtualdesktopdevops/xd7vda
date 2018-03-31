ea0c82e0062eef97fe1719183b3f2cebd9749b5e
matt6697 <32440697+matt6697@users.noreply.github.com>
2018-03-31 17:59:36 +0200

    Revert "Updated network interface config and puppet dependancies"
    This reverts commit 0b6a7ae49699f61d0b10110dd6c51b3881ff2126.

--- manifests/w10networkconfig.pp
+++ manifests/w10networkconfig.pp
@@ -1,19 +1,17 @@
 #Class configuring network for a Windows 10 Desktop VDA
 class xd7vda::w10networkconfig inherits xd7vda {
   if ($facts['osfamily'] == 'windows') and ($facts['os']['release']['major']==10) {
-    $facts['networking']['interfaces'].each |$netinterface| {
-      #Disable NETBIOS
-      dsc_xnetbios{"DisableNetBIOS_${netinterface}":
-        dsc_interfacealias => $netinterface,
-        dsc_setting        => 'Disable'
-      }
+    #Disable NETBIOS
+    dsc_xnetbios{'DisableNetBIOS':
+      dsc_interfacealias => 'Ethernet',
+      dsc_setting        => 'Disable'
+    }
 
-      #Disable IPV6
-      dsc_xnetadapterbinding{"DisableIPv6_${netinterface}":
-        dsc_interfacealias => $netinterface,
-        dsc_componentid    => 'ms_tcpip6',
-        dsc_state          => 'Disabled'
-      }
+    #Disable IPV6
+    dsc_xnetadapterbinding{'DisableIPv6':
+      dsc_interfacealias => 'Ethernet',
+      dsc_componentid    => 'ms_tcpip6',
+      dsc_state          => 'Disabled'
     }
   }
 }
--- manifests/w2k12r2networkconfig.pp
+++ manifests/w2k12r2networkconfig.pp
@@ -1,19 +1,17 @@
 #Configure Windows 2012R2 VDA network adapter
 class xd7vda::w2k12r2networkconfig inherits xd7vda {
   if ($facts['osfamily'] == 'windows') and ($facts['os']['release']['full']== '2012 R2') {
-    $facts['networking']['interfaces'].each |$netinterface| {
-      #Disable NETBIOS
-      dsc_xnetbios{"DisableNetBIOS_${netinterface}":
-        dsc_interfacealias => $netinterface,
-        dsc_setting        => 'Disable'
-      }
+    #Disable NETBIOS
+    dsc_xnetbios{'DisableNetBIOS':
+      dsc_interfacealias => 'Ethernet',
+      dsc_setting        => 'Disable'
+    }
 
-      #Disable IPV6
-      dsc_xnetadapterbinding{"DisableIPv6_${netinterface}":
-        dsc_interfacealias => $netinterface,
-        dsc_componentid    => 'ms_tcpip6',
-        dsc_state          => 'Disabled'
-      }
+    #Disable IPV6
+    dsc_xnetadapterbinding{'DisableIPv6':
+      dsc_interfacealias => 'Ethernet',
+      dsc_componentid    => 'ms_tcpip6',
+      dsc_state          => 'Disabled'
     }
   }
 }
