ee7f9115e3f7d3b36948bca61c18cf6efcdb8708
matt6697 <32440697+matt6697@users.noreply.github.com>
2018-03-31 18:05:09 +0200

    Using network interface name from facts, updated dependancies and puppet version requirement

--- manifests/w10networkconfig.pp
+++ manifests/w10networkconfig.pp
@@ -3,13 +3,13 @@ class xd7vda::w10networkconfig inherits xd7vda {
   if ($facts['osfamily'] == 'windows') and ($facts['os']['release']['major']==10) {
     #Disable NETBIOS
     dsc_xnetbios{'DisableNetBIOS':
-      dsc_interfacealias => 'Ethernet',
+      dsc_interfacealias => $facts['interfaces'],
       dsc_setting        => 'Disable'
     }
 
     #Disable IPV6
     dsc_xnetadapterbinding{'DisableIPv6':
-      dsc_interfacealias => 'Ethernet',
+      dsc_interfacealias => $facts['interfaces'],
       dsc_componentid    => 'ms_tcpip6',
       dsc_state          => 'Disabled'
     }
